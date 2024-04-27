90e220d0c0e26ef900fd1145da2e4b01afe9e13b
Nikolas-Vardeberg <post@nikolas.no>
2024-04-27 19:15:31 +0200

    css: new button variants

---
+++ app/buttons/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const ButtonsPage = () => {
+    return(
+        <div>
+            <Button>Primary</Button>
+        </div>
+    )
+}
+
+export default ButtonsPage
