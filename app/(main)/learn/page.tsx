3678ad8df2b0eb1841325a7c3ed24bb9c73e9719
Nikolas-Vardeberg <post@nikolas.no>
2024-04-28 12:43:21 +0200

    update: adding component and layout for /learn

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,11 @@
+
+
+const page = () => {
+    return(
+        <div>
+            Learn
+        </div>
+    )
+}
+
+export default page
---
+++ components/sidebar.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+
+type Props = {
+    className?: string;
+};
+
+export const Sidebar = ({className}: Props) => {
+    return(
+        <div className={cn("flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-2 flex-col", className)}>
+            <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                <Image 
+                    src="/mascot.svg" height={40} width={40}
+                    alt="Mascot"
+                />
+                <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">Lingo</h1>
+            </div>
+        </div>
+    )
+}
