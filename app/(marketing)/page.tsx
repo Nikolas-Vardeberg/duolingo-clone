db4ec5a29bc656a0c39aedcafcb14a2bbc5940f3
Nikolas-Vardeberg <post@nikolas.no>
2024-04-28 12:23:33 +0200

    update: clerk downgrade @4.92

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedOut } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedOut, SignedIn } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -13,7 +14,7 @@ export default function Home() {
             <div className="flex flex-col items-center gap-y-8">
                 <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">Learn, practive, and master new languages with Lingo.</h1>
 
-                <div>
+                <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
                     <ClerkLoading>
                         <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
                     </ClerkLoading>
@@ -34,6 +35,13 @@ export default function Home() {
                                 <Button size="lg" variant="primaryOuline" className="w-full">I already have an account</Button>
                             </SignInButton>
                         </SignedOut>
+                        <SignedIn>
+                            <Button size="lg" variant="secondary" className="w-full">
+                                <Link href="/learn">
+                                    Continue Learning
+                                </Link>
+                            </Button>
+                        </SignedIn>
                     </ClerkLoaded>
                 </div>
             </div>
