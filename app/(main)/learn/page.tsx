ba44d75e237f090820efdcc07948dc3472ed048a
Nikolas-Vardeberg <post@nikolas.no>
2024-05-06 19:27:41 +0200

    feat: Update user progress and units fetching logic

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,17 +2,20 @@ import { FeedWrapper } from "@/components/feed-wrapper"
 import { StickyWrapper } from "@/components/sticky-wrapper"
 import { Header } from "./header"
 import { UserProgress } from "@/components/user-progress"
-import { getUserProgress } from "@/db/queries"
+import { getUnits, getUserProgress } from "@/db/queries"
 import { redirect } from "next/navigation"
 
 
 const page = async() => {
     const userProgressData = getUserProgress();
+    const unitsData = getUnits();
 
     const [
-        userProgress
+        userProgress,
+        units
     ] = await Promise.all([
-        userProgressData
+        userProgressData,
+        unitsData
     ])
 
     if (!userProgress || !userProgress.activeCourse) {
@@ -23,14 +26,19 @@ const page = async() => {
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
                 <UserProgress
-                    activeCource={{ title: "Spanish", imageSrc: "/es.svg" }}
-                    hearts={5}
-                    points={100}
+                    activeCource={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
                     hasActiveSubscription={false}
                 />
             </StickyWrapper>
             <FeedWrapper>
-                <Header title="Spanish"/>
+                <Header title={userProgress.activeCourse.title} />
+                {units.map((unit) => (
+                    <div key={unit.id} className="mb-10">
+                        {JSON.stringify(unit)}
+                    </div>
+                ))}
             </FeedWrapper>
         </div>
     )
--- db/queries.ts
+++ db/queries.ts
@@ -3,7 +3,7 @@ import { cache} from "react"
 import db from "@/db/drizzle"
 import { auth } from "@clerk/nextjs"
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "./schema";
+import { challengeProgress, challenges, courses, units, userProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
     const { userId } = await auth();
@@ -26,6 +26,44 @@ export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
 
     return data;
+})
+
+export const getUnits = cache(async () => {
+    const userProgress = await getUserProgress();
+
+    if(!userProgress?.activeCourseId) {
+        return [];
+    }
+
+    const data = await db.query.units.findMany({
+        where: eq(units.courseId, userProgress.activeCourseId),
+        with: {
+            lesson: {
+                with: {
+                    challenges: {
+                        with: {
+                            challengeProgress: true,
+                        },
+                    },
+                },
+            },
+        },   
+    });
+
+    const normalizedData = data.map((unit) => {
+        const lessonsWithCompletedStatus = unit.lesson.map((lesson) => {
+            const allCompletedChallenges = lesson.challenges.every((challenges) => {
+                return challenges.challengeProgress && challenges.challengeProgress.length > 0
+                && challenges.challengeProgress.every((progress) => progress.completed);
+            })
+
+            return {...lesson, completed: allCompletedChallenges}
+        })
+
+        return { ...unit, lessons: lessonsWithCompletedStatus}
+    })
+
+    return normalizedData;
 })
 
 export const getCourseById = cache(async (courseId: number) => {
